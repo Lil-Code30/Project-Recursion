6cce07608188471d829f07c63a4d11f1a031dd10
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-04-23 15:41:45 -0400

    Merge sort using recursion method final complete version

--- mergesort.js
+++ mergesort.js
@@ -6,6 +6,35 @@ function mergeSort(arr) {
     const leftHalf = arr.slice(0, middle);
     const rightHalf = arr.slice(middle);
 
-    return mergeSort(leftHalf).concat(mergeSort(rightHalf));
+    // recursively sort the two halves
+    const sortedLeftHalf = mergeSort(leftHalf);
+    const sortedRightHalf = mergeSort(rightHalf);
+
+    // create an empty array to store the sorted elements
+    const sortedArray = [];
+
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    // sort and merge the two halves into the sorted array
+    while (i < sortedLeftHalf.length && j < sortedRightHalf.length) {
+      if (sortedLeftHalf[i] < sortedRightHalf[j]) {
+        sortedArray[k++] = sortedLeftHalf[i++];
+      } else {
+        sortedArray[k++] = sortedRightHalf[j++];
+      }
+    }
+
+    // add remaining elements of the left half to the sorted array
+    for (; i < sortedLeftHalf.length; i++) {
+      sortedArray[k++] = sortedLeftHalf[i];
+    }
+
+    // add remaining elements of the right half to the sorted array
+    for (; j < sortedRightHalf.length; j++) {
+      sortedArray[k++] = sortedRightHalf[j];
+    }
+    return sortedArray;
   }
 }
